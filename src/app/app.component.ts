02e000f9cdb138cdba8ccce334c3041fb5a076ba
NGUYEN Hoang Duc <nghoangduc@outlook.com>
2020-11-19 18:55:36 +0700

    i like to move it move it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'star';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('squareBox') squareBox: ElementRef;
+  transitionSpeed = 1000; // milisecond
+
+  constructor(
+    private renderer: Renderer2
+  ) { }
+
+  ngAfterViewInit = () => {
+    this.applySpeed();
+  }
+
+  applySpeed = () => {
+    this.renderer.setStyle(this.squareBox.nativeElement, 'transition-duration', `${this.transitionSpeed}ms`);
+  }
+
+  doTheThing = (event) => {
+    const {layerX, layerY} = event;
+    this.renderer.setStyle(this.squareBox.nativeElement, 'background', `#${Math.floor(Math.random() * 16777215).toString(16)}`);
+    this.renderer.setStyle(this.squareBox.nativeElement, 'top', `${layerY}px`);
+    this.renderer.setStyle(this.squareBox.nativeElement, 'left', `${layerX}px`);
+  }
 }
